724b25902208f171b847a7abdc3a4dc635df4e9e
Alejandro Steinmetz <asteinmetz78@gmail.com>
2019-10-29 14:56:22 -0300

    Demo app

---
+++ src/components/ClearInfo.vue
@@ -0,0 +1,18 @@
+<template>
+    <ion-button 
+        color="light" 
+        expand="block"
+        v-if="info"
+        @click="$emit('clear-info')"
+        >
+        Limpiar
+    </ion-button>
+</template>
+
+<script>
+export default {
+    name: "ClearInfo",
+    props: ["info"]
+}
+</script>
+
